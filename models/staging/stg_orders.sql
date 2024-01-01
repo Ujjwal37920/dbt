de3bf1611f2deeb4ee2a52a13cdf76688b395e8c
ujjwalkumar37920@gmail.com <ujjwalkumar37920@gmail.com>
2024-01-01 22:04:52 +0000

    Add report_profit by customer and report_profit_by_product

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,7 +1,9 @@
 select o.orderid, o.orderdate, o.shipdate, o.shipmode, o.ordercostprice, o.ordersellingprice, o.ordersellingprice - o.ordercostprice as orderprofit ,
 c.customername,
+c.customerid,
 c.segment,
 c.country,
+p.productid,
 p.category,
 p.productname,
 p.subcategory
